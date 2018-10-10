ebd6b2b591a13157d25ea80b73645b6f2304011e
Andreas Madsen <amwebdk@gmail.com>
2018-10-10 11:06:46 +0200

    only indirectly depened on tensorflow
    This handles tensorflow as a peer-dependency
    the same way bable does peer dependencies.
    By letting the dependent install it. In this
    case they have to install it anyway.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const semver = require('semver');
+const tfrange = require('./package.json')['devDependencies']['@tensorflow/tfjs-core'];
+
+let tf;
+try {
+  tf = require('@tensorflow/tfjs-core');
+} catch {
+  exit('@tensorflow/tfjs-core could not be found');
+}
+
+if (!semver.satisfies(tf.version_core, tfrange)) {
+  exit(`version ${tf.version_core} of @tensorflow/tfjs-core did not match ${tfrange}`);
+}
+
+function exit(condition) {
+  console.error(condition);
+  console.error(`Please run: npm install --save @tensorflow/tfjs-core@"${tfrange}"`);
+  process.exit(1);
+}
+
+module.exports = require('./lib/hmm.js');
